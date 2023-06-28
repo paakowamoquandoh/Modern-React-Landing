a21fb0255e89c0def8a5746c0c6f05764ab589e0
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-28 20:58:56 +0000

    Update App.js

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './App.css';
-import { Article, Brand, CTA } from "./components";
+import { Article, Brand, CTA, Navbar } from "./components";
 import { Blog, Features, Footer, Header, Prospects, WhyUs } from "./containers";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellooooooooooooo</h1>
+      <div className="gradientBg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhyUs/>
+      <Features/>
+      <Prospects/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
