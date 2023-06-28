0536f891e28dc9db9fbc097fb7861d7e2720a27e
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-06-28 03:56:20 +0000

    containers added

---
+++ landing/src/containers/prospects/Prospects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./prospect.css";
+
+const Prospects = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Prospects
---
+++ landing/src/containers/whyus/WhyUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./whyus.css";
+
+const WhyUs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WhyUs
